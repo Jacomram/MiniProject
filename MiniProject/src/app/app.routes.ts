268145944f3ba79ea0185d2c77fe131205b9f160
Jacobo <jacobojoseramirez@gmail.com>
2025-03-18 20:16:26 -0700

    Created header component

--- MiniProject/src/app/app.routes.ts
+++ MiniProject/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'tasks', loadComponent: () => import('./components/tasks/tasks.component').then(m => m.TasksComponent) },
+  { path: 'about', loadComponent: () => import('./components/about-us/about-us.component').then(m => m.AboutUsComponent) },
+  { path: 'login', loadComponent: () => import('./components/log-in/log-in.component').then(m => m.LogInComponent) },
+  { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) }
+];
