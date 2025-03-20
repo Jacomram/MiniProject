282c1213e6e728e559c3d39c771c577cf432d387
Jacobo <jacobojoseramirez@gmail.com>
2025-03-20 02:47:20 -0700

    Header component Structure and functionality: a) using material.io nav bar setup b) routing when we click on home,login - it should goes to login/register

--- MiniProject/src/app/components/header/header.component.ts
+++ MiniProject/src/app/components/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
+    standalone: true,
+    imports: [RouterLink],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
