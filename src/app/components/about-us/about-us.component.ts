6e6ed47adecb578b278b1311280d61839a210f0e
Maria Castro <mvcastrotrujillo@gmail.com>
2025-03-25 19:29:34 -0700

    It's the image that shows the team members of the project , also edited the page for the about us where I write a very brief description of us and show our emails for future contact of any of the members.

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SideNavListComponent } from '../side-nav/side-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SideNavListComponent, MatSidenavModule],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrls: ['./about-us.component.css'] // Corregido: styleUrls en plural
 })
 export class AboutUsComponent {
+    sidenavOpened = false;
 
+    toggleSidenav() {
+      this.sidenavOpened = !this.sidenavOpened;
+    }
 }
