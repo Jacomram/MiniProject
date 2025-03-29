7cbf8658164f437e8157d061f642edbbc77b9eb9
Donovan Jimenez Mendez <cris.jdonovan@gmail.com>
2025-03-28 19:59:47 -0700

    Finish products and Invoice page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,7 @@ export const routes: Routes = [
   { path: 'about', loadComponent: () => import('./components/about-us/about-us.component').then(m => m.AboutUsComponent) },
   { path: 'login', loadComponent: () => import('./components/log-in/log-in.component').then(m => m.LogInComponent) },
   { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) },
-  { path: 'profile',loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent) },
+  { path: 'profile', loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent) },
+  { path: 'invoice', loadComponent: () => import('./components/invoice/invoice.component').then(m => m.InvoiceComponent) },
+  { path: 'products', loadComponent: () => import('./components/products/products.component').then(m => m.ProductsComponent) },
 ];
